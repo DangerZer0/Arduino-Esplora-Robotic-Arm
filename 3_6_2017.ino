417a22c4c62e11dee86c2824053a4ddd18ee5def
DangerZer0 <liubomir1998@yahoo.com>
2017-06-04 10:44:37 +0300

    Update 3_6_2017.ino

--- 3_6_2017.ino
+++ 3_6_2017.ino
@@ -9,7 +9,14 @@
 #include <Servo.h>
 #include <Esplora.h>
 
-Servo myservoX, myservoY, myservoZ, myservoH;  // Create 4 servo objects to control the servos connected to the Arduino Esplora
+/*
+Create 4 servo objects to control the servos connected to the Arduino Esplora
+- myservoX: X axis servo motor
+- myservoY: Y axis servo motor
+- myservoZ: Z axis servo motor
+- myservoH: Hand grab servo motor
+*/
+Servo myservoX, myservoY, myservoZ, myservoH;  
 
 int xDefServ,  yDefServ, zDefServ;   // Default servo location, in degrees (value from 0 to 180)
 int xCurrServ, yCurrServ, zCurrServ;  // Current servo location, in degrees (value from 0 to 180)
@@ -18,7 +25,7 @@ int xNewServ,  yNewServ, zNewServ;   // New servo location to reach, in degrees 
 int xCurrJoy, yCurrJoy, zCurrJoy;    // Current joystick position, in range (from -512 to +512)
 int xCalJoy, yCalJoy, zCalJoy;      // Calibrate offset on the joystick position, in range (from -512 to +512)
 
-int calibBtnState = 0, calibBtnDuration = 0;  // Variables used in statemachine for the calibration button status
+int calibBtnState = 0, calibBtnDuration = 0;  // Calibration button status buttons
 float stepUp, sysSpeed;                       // The update speed of the current X,Y Coordinates
 int calibSpeed;                               // The speed of calibration process
 int pulseCount, pulseComp;                    // Used to emulate servo PWM signal on output digital pin no. 1
@@ -27,50 +34,51 @@ long debounce = 200;   // The debounce time, increase if the output flickers
 int state = HIGH;      // The current state of the output pin
 int previous = LOW;    // The previous reading from the input pin
 long time = 0;         // The last time the output pin was toggled
+
 long debounce1 = 200;   // The debounce time, increase if the output flickers
 int state1 = HIGH;      // The current state of the output pin
 int previous1 = LOW;    // The previous reading from the input pin
 
-void setup()
-{
+
+void setup(){
   xDefServ = 90;              // Default servo X angle
   yDefServ = 180;              // Default servo Y angle
-  zDefServ = 180;              // Default servo Z angle
+  zDefServ = 120;              // Default servo Z angle
   
   myservoX.attach(11);        // Initialize first servo on D11
   myservoX.write(xDefServ);   // Set servo to default X position
-  myservoY.attach(3);         // Initialize first servo on D3
+  myservoY.attach(3);         // Initialize second servo on D3
   myservoY.write(yDefServ);   // Set servo to default Y position
-  myservoH.attach(0);
+  myservoZ.attach(1);         // Initialize third servo on D1
+  myservoZ.write(zDefServ);   // Set servo to default Z position
+  myservoH.attach(0);         // Initialize forth servo on D0
+  
   xCurrServ = xDefServ;       // Set the current X Position
   yCurrServ = yDefServ;       // Set the current Y Position
-  zCurrServ = zDefServ;        // Set the current Z Position
+  zCurrServ = zDefServ;       // Set the current Z Position
   xNewServ = xDefServ;        // Set the new X Position
   yNewServ = yDefServ;        // Set the new Y Position
   zNewServ = zDefServ;        // Set the new Z position
 
   xCalJoy = Esplora.readJoystickX();    // Set the calibration for the X joystick offet
   yCalJoy = Esplora.readJoystickY();    // Set the calibration for the Y joystick offet
+  zCalJoy = Esplora.readJoystickY();    // Set the calibration for the Z joystick offet
   xCurrJoy = map(xCurrServ, 0, 180, -512, 512) - xCalJoy;   // Set the current joystick X reference position
-  yCurrJoy = map(yCurrServ, 180, 0, -512, 512) - xCalJoy;   // Set the current joystick Y reference position
-  zCurrJoy = map(zCurrServ, 0, 180, -512, 512) - xCalJoy;   // Set the current joystick Z reference position 
+  yCurrJoy = map(yCurrServ, 0, 180, -512, 512) - xCalJoy;   // Set the current joystick Y reference position
+  zCurrJoy = map(zCurrServ, 0, 180, -512, 512) - xCalJoy;   // Set the current joystick Z reference position8 
   
-
   stepUp = 0;               // Initialize the update speed
-  pinMode(1, OUTPUT);       // Define the difital pin D1 as output pin, used to control the head tilt servo
-  pinMode(0, OUTPUT);       // Define the digital pin D0 as output pin, used to control the head grab servo
-  pulseCount = 0;           // Initialize the variable representing the pulse width for the shutter servo
-  pulseComp = 0;            // Initialize the variable representing the pulse duration for the shutter servo
+  pinMode(0, OUTPUT);       // Define the digital pin D0 as output pin, used to control the head grab servo motor
   sysSpeed = map(Esplora.readSlider(), 0, 1023, 1, 8) / 4.0;     // The speed of updating position
 }
-
 
 
 void reCalibrate(){
   // Set the servos' default position to the current position they hold.
   // This function works on the values in degrees
-  xDefServ = map(xCurrJoy, -512, 512, 0, 180) ;        // Set the default X position, in degree
-  yDefServ = map(yCurrJoy, -512, 512, 0, 180) ;        // Set the default Y position, in degree
+  xDefServ = map(xCurrJoy, -512, 512, 0, 180);        // Set the default X position, in degree
+  yDefServ = map(yCurrJoy, -512, 512, 0, 180);        // Set the default Y position, in degree
+  zDefServ = map(zCurrJoy, -512, 512, 0, 180);        // Set the default Z position, in degree
 }
 
 
@@ -99,26 +107,35 @@ void backToDef() {
       yCurrServ = yCurrServ - stepUp;
     myservoY.write(yCurrServ);
     delay(calibSpeed);
-   }
-   // Calucalte the X,Y current joystick positions
-   xCurrJoy = map(xCurrServ, 0, 180, -512, 512);
-   yCurrJoy = map(yCurrServ, 180, 0, -512, 512);
+
+    //move the Z servo
+    if (zNewServ > zCurrServ)
+      zCurrServ = zCurrServ + stepUp;
+    else if (zNewServ < zCurrServ)
+      zCurrServ = zCurrServ - stepUp;
+    myservoZ.write(zCurrServ);
+    delay(calibSpeed);
+  }
+  // Calucalte the X, Y, Z current joystick positions
+  xCurrJoy = map(xCurrServ, 0, 180, -512, 512);
+  yCurrJoy = map(yCurrServ, 0, 180, -512, 512);
+  zCurrJoy = map(yCurrServ, 0, 180, -512, 512);
 }
+
 
 // Main Loop
 void loop(){
-  
   sysSpeed = map(Esplora.readSlider(), 0, 1023, 1, 8) / 4.0;    // Calculate the system speed based on the Esplora slider
-
-  int reading1 = Esplora.readButton(SWITCH_RIGHT);
   
   stepUp = (-1.0 * map(Esplora.readJoystickX() - xCalJoy, -512, 512, -5, 5)) * sysSpeed;  // Calculate the step for the X joystick coordinate
   if (stepUp > 0)
     xCurrJoy = min(xCurrJoy + stepUp, 512);  // To avoid X position overflow
   else
     xCurrJoy = max(xCurrJoy + stepUp, -512); // To avoid X position downflow
-        
-  if (reading1 == HIGH && previous1 == LOW && millis() - time > debounce1) {
+    
+  int reading1 = Esplora.readButton(SWITCH_RIGHT);    
+    
+  if (reading1 == LOW && previous1 == HIGH && millis() - time > debounce1) {
     
     if (state1 == HIGH){
       stepUp = (-1.0 * map(Esplora.readJoystickY() - yCalJoy, -512, 512, -5, 5)) * sysSpeed; // Calculate the step for the Y joystick coordinate
@@ -141,7 +158,7 @@ void loop(){
   previous1 = reading1;
 
   myservoX.write(map(xCurrJoy, -512, 512, 0, 180));   // Write the X position to the servo
-  myservoY.write(map(yCurrJoy, -512, 512, 180, 0));   // Write the Y position to the servo
+  myservoY.write(map(yCurrJoy, -512, 512, 0, 180));   // Write the Y position to the servo
   myservoZ.write(map(zCurrJoy, -512, 512, 0, 180));   // Write the Z position to the servo
  
   switch (calibBtnState){
